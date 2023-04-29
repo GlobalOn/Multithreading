ee862752fb46d6755285efb7531569d56d010b10
urkin <urkin961@gmail.com>
2023-04-29 14:37:57 +0200

    Threading-fundamentals---Thread-Creation    Two ways of Threads creation was refreshed Simple game with second approach was created.

---
+++ src/VaultMultithreadingGame.java
@@ -0,0 +1,104 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class VaultMultithreadingGame {
+    public static final int MAXPASS = 9999;
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        Vault vault = new Vault(random.nextInt(MAXPASS));
+        List<Thread> threads = new ArrayList<>();
+
+        threads.add(new AscendingHacker(vault));
+        threads.add(new DescendingHacker(vault));
+        threads.add(new Sheriff());
+
+        for (Thread thread :
+                threads) {
+            thread.start();
+        }
+    }
+
+    private static class Vault {
+        private int password;
+
+        public Vault(int password) {
+            this.password = password;
+        }
+
+        public boolean isCorrectPassword(int guess) {
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return this.password == guess;
+        }
+    }
+
+    public static abstract class HackerThread extends Thread {
+        protected Vault vault;
+
+        public HackerThread(Vault vault) {
+            this.vault = vault;
+            this.setName(this.getClass().getSimpleName());
+            this.setPriority(MAX_PRIORITY);
+        }
+
+        @Override
+        public void start() {
+            System.out.println("Starting thread " + this.getName());
+            super.start();
+        }
+    }
+
+    private static class AscendingHacker extends HackerThread {
+        public AscendingHacker(Vault vault) {
+            super(vault);
+        }
+
+        @Override
+        public void run() {
+            for (int guess = 0; guess < MAXPASS; guess++) {
+                if (vault.isCorrectPassword(guess)) {
+                    System.out.println(this.getName() + " guessed the password " + guess);
+                    System.exit(0);
+                }
+            }
+        }
+    }
+
+    private static class DescendingHacker extends HackerThread {
+        public DescendingHacker(Vault vault) {
+            super(vault);
+        }
+
+        @Override
+        public void run() {
+            for (int guess = MAXPASS; guess > 0; guess--) {
+                if (vault.isCorrectPassword(guess)) {
+                    System.out.println(this.getName() + " guessed the password " + guess);
+                    System.exit(0);
+                }
+            }
+        }
+    }
+
+    private static class Sheriff extends Thread {
+        @Override
+        public void run() {
+            for (int i = 10; i > 0; i--) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(i);
+            }
+            System.out.println("Game Over!");
+            System.exit(0);
+        }
+    }
+
+}
